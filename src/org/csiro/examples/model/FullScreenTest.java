d9e99b5f72318a16ac8a72e3fa37d89e4d0dd6d8
whatnickd <whatnickd@localhost>
2011-02-14 14:44:19 +1100

    ENH: Added animation to the full screen demo

--- src/org/csiro/examples/model/FullScreenTest.java
+++ src/org/csiro/examples/model/FullScreenTest.java
@@ -1,47 +1,36 @@
 package org.csiro.examples.model;
 
 import gov.nasa.worldwind.BasicModel;
+import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
 import gov.nasa.worldwind.examples.ApplicationTemplate;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.view.firstperson.BasicFlyView;
+import gov.nasa.worldwind.view.firstperson.FlyToFlyViewAnimator;
 
-import java.awt.Dimension;
 import java.awt.Frame;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsDevice;
 import java.awt.event.KeyEvent;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class FullScreenTest extends ApplicationTemplate {
 
-	public static class AppFrame extends ApplicationTemplate.AppFrame {
-		public AppFrame() {
-			super(true, false, false);
-			this.getWwd().setModel(new BasicModel());
-			GraphicsConfiguration config = this.getGraphicsConfiguration();
-			GraphicsDevice device = config.getDevice();
-
-			if(device.isFullScreenSupported())
-			{
-				try {
-					this.setUndecorated(true);
-			        this.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
-					this.setResizable(false);
-					this.getWwd().setPreferredSize(new Dimension(1000,800));
-					device.setFullScreenWindow(this);
-					
-				} finally {
-					device.setFullScreenWindow(null);
-				}
-			}
-		}
-	}
+	/**
+	 * The animated view
+	 */
+	public static BasicFlyView view;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		view = new BasicFlyView();
 		Frame frame = new Frame("WorldwindFull");
 		final WorldWindowGLCanvas worldWindowGLCanvas = new WorldWindowGLCanvas();
 		worldWindowGLCanvas.setModel(new BasicModel());
+		worldWindowGLCanvas.setView(view);
 
 		worldWindowGLCanvas.addKeyListener(new java.awt.event.KeyListener() {
 			public void keyTyped(KeyEvent e) {
@@ -66,5 +55,42 @@ public class FullScreenTest extends ApplicationTemplate {
 
 		frame.setVisible(true);
 		worldWindowGLCanvas.requestFocus();
+		
+		setUpTimer();
+	}
+
+	public static void moveToLocation(Position pos) {
+		if (pos == null) {
+			return;
+		}
+		double elevation = view.getGlobe().getElevation(pos.getLatitude(),
+				pos.getLongitude());
+		FlyToFlyViewAnimator animator = FlyToFlyViewAnimator
+				.createFlyToFlyViewAnimator(view, view.getEyePosition(),
+						new Position(pos.latitude, pos.longitude, elevation),
+						view.getHeading(), view.getHeading(), view.getPitch(),
+						view.getPitch(), view.getEyePosition().getElevation(),
+						view.getEyePosition().getElevation(), 10000, true);
+		view.addAnimator(animator);
+		animator.start();
+		view.firePropertyChange(AVKey.VIEW, null, view);
+	}
+
+	public static void setUpTimer()
+	{
+		int delay = 5000;   // delay for 5 sec.
+		int period = 10000;  // repeat every sec.
+		Timer timer = new Timer();
+
+		timer.scheduleAtFixedRate(new TimerTask() {
+		        public void run() {
+		        	Position pos = new Position(Angle.fromDegrees(
+		        			180*new Random().nextFloat()-90.0),
+		        								Angle.fromDegrees(
+		        			360*new Random().nextFloat()-180.0),
+		        								0.0);
+		            moveToLocation(pos);
+		        }
+		    }, delay, period);
 	}
 }
