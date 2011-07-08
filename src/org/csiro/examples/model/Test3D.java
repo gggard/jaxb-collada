c1bf22ed0b66271f80b5a7bc804fcc931b2c7786
whatnickd <whatnickd@localhost>
2011-07-08 10:05:43 +1000

    ADD: Nicer OBJ model (with failing normals count test)

--- src/org/csiro/examples/model/Test3D.java
+++ src/org/csiro/examples/model/Test3D.java
@@ -11,77 +11,75 @@ import java.util.Random;
 
 public class Test3D extends ApplicationTemplate {
 
-    public static class AppFrame extends ApplicationTemplate.AppFrame {
+	public static class AppFrame extends ApplicationTemplate.AppFrame {
 
-        public AppFrame() {
-            super(true, true, false);
-            try {
-                RenderableLayer layer = new RenderableLayer();
-                layer.setName("movable 3D object");
-                
-                Random generator = new Random();
-                /*
-                for (int i = 0; i < 1; i++) {
-                    layer.addRenderable(
-                            new Movable3DModel(
-                            "./3dmodels/spaceship.3ds",
-                            new Position(Angle.fromDegrees(generator.nextInt() % 90),
-                            Angle.fromDegrees(generator.nextInt() % 180), 200000),
-                            300000));
-                }
-                */
-                /*
-                Movable3DModel superdome = new Movable3DModel(
-                        "./testmodels/models/model.dae",
-                        new Position(Angle.fromDegrees(-34.940679766700),
-                        		Angle.fromDegrees(138.623116628920), 70),
-                        1.0);
-                superdome.setUseArdor(true);
-                layer.addRenderable(superdome);
-                */
-                /*
-                Movable3DModel macquarie = new Movable3DModel(
-                        "./testmodels/models/untitled.dae",
-                        new Position(Angle.fromDegrees(-42.88306855273),
-                        		Angle.fromDegrees(147.3295738186), 25),
-                        0.1);
-                macquarie.setUseArdor(true);
-                layer.addRenderable(macquarie);
-                */
-                
-                Ardor3DModel model = new Ardor3DModel(
-                		"./testmodels/drykovanov/models/untitled.dae",
-                        new Position(Angle.fromDegrees(-42.88306855273),
-                        		Angle.fromDegrees(147.3295738186), 25));
-                
-                layer.addRenderable(model);
-                
-                Ardor3DModel airap = new Ardor3DModel(
-                        "./testmodels/models/model.dae",
-                        new Position(Angle.fromDegrees(-34.940679766700),
-                        		Angle.fromDegrees(138.623116628920), 70));
-                
-                layer.addRenderable(airap);
-                
-                /*
-                layer.addRenderable(
-                        new Movable3DModel(
-                        "./3dmodels/catharsis2manta.3ds",
-                        new Position(Angle.fromDegrees(-35.0),
-                        		Angle.fromDegrees(138.5), 200000),
-                        300000));
-                */
-                this.getWwd().addSelectListener(new GenericDraggerAdjuster(this.getWwd()));
-                insertBeforeCompass(this.getWwd(), layer);
-                this.getLayerPanel().update(this.getWwd());                
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+		public AppFrame() {
+			super(true, true, false);
+			try {
+				RenderableLayer layer = new RenderableLayer();
+				layer.setName("movable 3D object");
 
-    public static void main(String[] args) {
-        ApplicationTemplate.start("Movable 3D Model Layer Test", AppFrame.class);
-    }
+				layer
+						.addRenderable(new Movable3DModel(
+								"./3dmodels/spaceship.3ds",
+								new Position(Angle
+										.fromDegrees(-34.940679766700), Angle
+										.fromDegrees(138.623116628920), 200000),
+								300000));
+
+				/*
+				 * Movable3DModel superdome = new Movable3DModel(
+				 * "./testmodels/models/model.dae", new
+				 * Position(Angle.fromDegrees(-34.940679766700),
+				 * Angle.fromDegrees(138.623116628920), 70), 1.0);
+				 * superdome.setUseArdor(true); layer.addRenderable(superdome);
+				 */
+				/*
+				 * Movable3DModel macquarie = new Movable3DModel(
+				 * "./testmodels/models/untitled.dae", new
+				 * Position(Angle.fromDegrees(-42.88306855273),
+				 * Angle.fromDegrees(147.3295738186), 25), 0.1);
+				 * macquarie.setUseArdor(true); layer.addRenderable(macquarie);
+				 */
+				/*
+				 * Ardor3DModel model = new Ardor3DModel(
+				 * "./testmodels/drykovanov/models/untitled.dae", new
+				 * Position(Angle.fromDegrees(-42.88306855273),
+				 * Angle.fromDegrees(147.3295738186), 25));
+				 * 
+				 * layer.addRenderable(model);
+				 */
+				Ardor3DModel airap = new Ardor3DModel(
+						"./testmodels/models/model.dae", new Position(Angle
+								.fromDegrees(-34.940679766700), Angle
+								.fromDegrees(138.623116628920), 70));
+
+				layer.addRenderable(airap);
+
+				layer.addRenderable(new Movable3DModel(
+						"./testmodels/sea_horse/sea_horse.obj", new Position(
+								Angle.fromDegrees(-42.88306855273), Angle
+										.fromDegrees(147.3295738186), 2000),
+						3000));
+
+				layer.addRenderable(new Movable3DModel(
+						"./testmodels/sea_horse/sea_horse.obj", new Position(
+								Angle.fromDegrees(-42.98306855273), Angle
+										.fromDegrees(147.3295738186), 2000),
+						3000));
+
+				this.getWwd().addSelectListener(
+						new GenericDraggerAdjuster(this.getWwd()));
+				insertBeforeCompass(this.getWwd(), layer);
+				this.getLayerPanel().update(this.getWwd());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		ApplicationTemplate
+				.start("Movable 3D Model Layer Test", AppFrame.class);
+	}
 }
-
