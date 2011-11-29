0cd20d520639f54a0ddc34618a7b0f84130cf53d
michael.dehoog <michael.dehoog@localhost>
2011-11-29 16:10:17 +1100

    Updated the CustomKMLParserContext to generate the new CustomKMLRegion class.

--- src/gov/nasa/worldwind/ogc/kml/custom/CustomKMLParserContext.java
+++ src/gov/nasa/worldwind/ogc/kml/custom/CustomKMLParserContext.java
@@ -1,22 +1,42 @@
 package gov.nasa.worldwind.ogc.kml.custom;
 
 import gov.nasa.worldwind.ogc.kml.KMLParserContext;
+import gov.nasa.worldwind.ogc.kml.custom.CustomKMLPlacemark;
 
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 
-public class CustomKMLParserContext extends KMLParserContext {
-
-	public CustomKMLParserContext(XMLEventReader eventReader,
-			String defaultNamespace) {
+/**
+ * Sets up a custom {@link KMLParserContext} that replaces certain element
+ * classes with our custom ones.
+ * 
+ * @author Tisham Dhar
+ * @author Michael de Hoog (michael.dehoog@ga.gov.au)
+ */
+public class CustomKMLParserContext extends KMLParserContext
+{
+	public CustomKMLParserContext(XMLEventReader eventReader, String defaultNamespace)
+	{
 		super(eventReader, defaultNamespace);
 	}
 
+	public CustomKMLParserContext(String defaultNamespace)
+	{
+		super(defaultNamespace);
+	}
+
+	public CustomKMLParserContext(CustomKMLParserContext ctx)
+	{
+		super(ctx);
+	}
 
 	@Override
-	protected void initializeParsers(String ns) {
+	protected void initializeParsers(String ns)
+	{
 		super.initializeParsers(ns);
 		this.parsers.remove(new QName(ns, "Placemark"));
 		this.parsers.put(new QName(ns, "Placemark"), new CustomKMLPlacemark(ns));
+		this.parsers.remove(new QName(ns, "Region"));
+		this.parsers.put(new QName(ns, "Region"), new CustomKMLRegion(ns));
 	}
 }
